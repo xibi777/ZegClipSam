0c725f55b05260aa2012c5d2f8a88e6d8d77efc3
ZiqinZhou66 <you@example.com>
2023-07-18 13:28:58 +0930

    add config

--- run_voc_vit_dgx.sh
+++ run_voc_vit_dgx.sh
@@ -1,8 +1,8 @@
-bash dist_train_01.sh configs/voc12_vit/vit_b_16_fully_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_16_fully_vpt
-bash dist_train_01.sh configs/voc12_vit/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_0
-bash dist_train_01.sh configs/voc12_vit/vit_b_16_split_1_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_1
-bash dist_train_01.sh configs/voc12_vit/vit_b_16_split_2_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_2
-bash dist_train_01.sh configs/voc12_vit/vit_b_16_split_3_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_3
+bash dist_train_0123.sh configs/voc12_vit/vit_b_16_fully_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_16_fully_vpt
+bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_0
+bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_1_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_1
+bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_2_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_2
+bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_3_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_3
 
 
 # bash dist_train_01.sh configs/voc12_vit_Fix_EL_RD/vit_b_16_fully.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_RD/vit_b_16_fully
