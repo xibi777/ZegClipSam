ca7471e5f41cd9d8ec8db68b36088f7207256e68
ZiqinZhou66 <you@example.com>
2023-11-12 17:58:44 +1030

    add voc capl config

--- run_voc_vit_dgx.sh
+++ run_voc_vit_dgx.sh
@@ -1,41 +1,4 @@
-# bash dist_train_0123.sh configs/voc12_vit/vit_b_16_fully_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_16_fully_vpt
-# bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_0
-# bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_1_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_1
-# bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_2_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_2
-# bash dist_train_0123.sh configs/voc12_vit/vit_b_16_split_3_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit/vit_b_16_split_3
-
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_RD/vit_b_16_fully.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_RD/vit_b_16_fully
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_RD/vit_b_16_split_0_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_RD/vit_b_16_split_0
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_RD/vit_b_16_split_1_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_RD/vit_b_16_split_1
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_RD/vit_b_16_split_2_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_RD/vit_b_16_split_2
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_RD/vit_b_16_split_3_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_RD/vit_b_16_split_3
-
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_WORD/vit_b_16_fully.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_WORD/vit_b_16_fully
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_WORD/vit_b_16_split_0_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_WORD/vit_b_16_split_0
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_WORD/vit_b_16_split_1_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_WORD/vit_b_16_split_1
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_WORD/vit_b_16_split_2_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_WORD/vit_b_16_split_2
-# bash dist_train_01.sh configs/voc12_vit_Fix_EL_WORD/vit_b_16_split_3_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_EL_WORD/vit_b_16_split_3
-
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_RD/vit_b_16_fully.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_RD/vit_b_16_fully
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_RD/vit_b_16_split_0_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_RD/vit_b_16_split_0
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_RD/vit_b_16_split_1_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_RD/vit_b_16_split_1
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_RD/vit_b_16_split_2_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_RD/vit_b_16_split_2
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_RD/vit_b_16_split_3_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_RD/vit_b_16_split_3
-
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_WORD/vit_b_16_fully.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_WORD/vit_b_16_fully
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_WORD/vit_b_16_split_0_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_WORD/vit_b_16_split_0
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_WORD/vit_b_16_split_1_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_WORD/vit_b_16_split_1
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_WORD/vit_b_16_split_2_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_WORD/vit_b_16_split_2
-# bash dist_train_01.sh configs/voc12_vit_Fix_NEL_WORD/vit_b_16_split_3_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc12_vit_Fix_NEL_WORD/vit_b_16_split_3
-
-bash dist_train_0123.sh configs/voc12_resnet50/resnet50_fully_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50/resnet50_fully
-bash dist_train_0123.sh configs/voc12_resnet50/resnet50_split_0_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50/resnet50_split_0
-bash dist_train_0123.sh configs/voc12_resnet50/resnet50_split_1_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50/resnet50_split_1
-bash dist_train_0123.sh configs/voc12_resnet50/resnet50_split_2_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50/resnet50_split_2
-bash dist_train_0123.sh configs/voc12_resnet50/resnet50_split_3_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50/resnet50_split_3
-
-bash dist_train_0123.sh configs/voc12_resnet50_word/resnet50_fully_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50_word/resnet50_fully
-bash dist_train_0123.sh configs/voc12_resnet50_word/resnet50_split_0_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50_word/resnet50_split_0
-bash dist_train_0123.sh configs/voc12_resnet50_word/resnet50_split_1_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50_word/resnet50_split_1
-bash dist_train_0123.sh configs/voc12_resnet50_word/resnet50_split_2_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50_word/resnet50_split_2
-bash dist_train_0123.sh configs/voc12_resnet50_word/resnet50_split_3_512x512_20k.py /media/data/ziqin/work_dirs_fss/voc_resnet50_word/resnet50_split_3
+bash dist_train_01.sh configs/voc12_vit_capl_prompt/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit_capl_prompt/vit_b_16_split_0
+bash dist_train_01.sh configs/voc12_vit_capl_prompt/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit_capl_prompt/vit_b_16_split_1
+bash dist_train_01.sh configs/voc12_vit_capl_prompt/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit_capl_prompt/vit_b_16_split_2
+bash dist_train_01.sh configs/voc12_vit_capl_prompt/vit_b_16_split_0_512x512_20k_12_10.py /media/data/ziqin/work_dirs_fss/voc_vit_capl_prompt/vit_b_16_split_3
